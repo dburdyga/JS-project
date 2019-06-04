ff7ecf7a84fc3a3f710ff173e3afa5e6ef5a9f33
Daria <dariaburdyga@gmail.com>
2019-06-04 12:31:01 +0300

    function createDivWithText done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
+    var div = document.createElement('div');
+    div.innerText = text;
+    return div;
 }
 
 /*
