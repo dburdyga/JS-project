bdd852e8f35538f828993da304e800b22065db67
Daria <dariaburdyga@gmail.com>
2019-06-07 11:54:45 +0300

    6 tests successful

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
-	target.addListener(eventName, fn);
+	target.addEventListener(eventName, fn);
 }
 
 /*
@@ -21,7 +21,7 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
-	target.removeListener(eventName, fn);
+	target.removeEventListener(eventName, fn);
 }
 
 /*
