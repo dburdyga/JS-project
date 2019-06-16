6b5729e1665dda04d629baf03cff8993434be987
Daria <dariaburdyga@gmail.com>
2019-06-16 13:28:37 +0300

    tests done

--- src/cookie.js
+++ src/cookie.js
@@ -6,6 +6,7 @@
    - значение
    - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
 
+
  7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
    - имя
    - значение
@@ -43,10 +44,54 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-filterNameInput.addEventListener('keyup', function() {
-	// здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+function getCookies() {
+	return document.cookie
+		.split('; ')
+		.filter(Boolean)
+		.map(cookie => cookie.match(/^([^=]+)=(.+)/))
+		.reduce((obj, [, name, value]) => {
+			obj[name] = value;
+
+			return obj;
+		}, {});
+}
+
+function isMatching(full, chunk) {
+	if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+renderTable();
+
+listTable.addEventListener('click', function (e) {
+	console.log(e.target);
+	if (e.target.dataset.key) {
+		document.cookie = e.target.dataset.key + `=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+	}
+	renderTable();
 });
 
-addButton.addEventListener('click', () => {
-	// здесь можно обработать нажатие на кнопку "добавить cookie"
+
+filterNameInput.addEventListener('keyup', function () {
+	renderTable();
 });
+
+
+addButton.addEventListener('click', () => {
+	document.cookie = `${addNameInput.value} = ${addValueInput.value}`;
+	renderTable();
+});
+
+
+function renderTable() {
+	const cookie_obj = getCookies();
+	listTable.innerHTML = '';
+	for (let key in cookie_obj) {
+		if(!(isMatching(key,filterNameInput.value) || isMatching(cookie_obj[key], filterNameInput.value))) continue;
+		listTable.innerHTML += `<tr><td class="first_td">${key}</td><td>${cookie_obj[key]}</td><td><button class="del" 
+        data-key="${key}">Удалить</button></td></tr>`;
+	}
+}
